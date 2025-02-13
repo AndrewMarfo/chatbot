d30457bc0f30edc7753b32cecab6462d6027882c
Andrew Marfo <andrewmarfo23@gmail.com>
2025-02-13 18:18:40 +0000

    refined the RAG pipeline

--- app.py
+++ app.py
@@ -1,12 +1,5 @@
 from model import ask_question
-from model import load_data
-from model import chunk_and_index
-from model import create_gemini_model
 import streamlit as st
-
-document = load_data()
-index = chunk_and_index(document)
-create_gemini_model()
 
 st.title("Paul Graham's Chatbot")
 prompt = st.chat_input("Ask Paul Graham a question")
