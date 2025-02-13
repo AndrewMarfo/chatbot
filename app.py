9474c7efa62b5d6df9d08f91194cf1577bd5e60a
Andrew Marfo <andrewmarfo23@gmail.com>
2025-02-13 22:51:32 +0000

    App UI redesigned

--- app.py
+++ app.py
@@ -1,8 +1,36 @@
 from model import ask_question
 import streamlit as st
+import time
 
-st.title("Paul Graham's Chatbot")
-prompt = st.chat_input("Ask Paul Graham a question")
+def stream_response(sentences):
+    for word in sentences.split(" "):
+        yield word + " "
+        time.sleep(0.03)
+
+st.title("Welcome to PaulChat 👋")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+prompt = st.chat_input("Ask Paul a question")
 if prompt:
-    answer = ask_question(prompt)
-    st.write(answer)
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    with st.spinner("Generating response..."):
+        response = ask_question(prompt)
+        # Display AI response in chat message container
+        with st.chat_message("assistant"):
+            st.write_stream(stream_response(response))
+
+        st.session_state.messages.append({"role": "assistant", "content": response})
+
